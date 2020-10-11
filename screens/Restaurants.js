e3588afb3b9124487989a5cc6ea35963223f40cc
Roser Reverte <reverroser@gmail.com>
2020-10-11 16:11:24 +0100

    feat(restaurants): filter restaurants by name

--- screens/Restaurants.js
+++ screens/Restaurants.js
@@ -1,25 +1,47 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 
 import restaurants from "../mocks/restaurants";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22
+    backgroundColor: "#fff"
   },
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44
+    padding: 16,
+    fontSize: 18
+  },
+  search: {
+    margin: 16,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    borderRadius: 8
   }
 });
 
 export default function Restaurants() {
+  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants);
+
+  const onSearchChange = text => {
+    setFilteredRestaurants(
+      restaurants.filter(({ name }) =>
+        name.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+      )
+    );
+  };
+
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.search}
+        onChangeText={onSearchChange}
+        allowFontScaling={true}
+        clearButtonMode={"always"}
+        placeholder={"Search restaurants"}
+      />
       <FlatList
-        data={restaurants}
+        data={filteredRestaurants}
         renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
         keyExtractor={(item, index) => index.toString()}
       />
